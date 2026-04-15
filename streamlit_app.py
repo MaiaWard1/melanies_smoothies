007102264d031d04eba506279e5290e192afcac1
MaiaWard1 <maiaward@kubrickgroup.com>
2026-04-15 10:33:11 +0100

    Update streamlit_app.py
    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,9 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
+
 cnx = st.connection("snowflake")
 session = cnx.session()
-
 
 ingredients_list = st.multiselect(
   'Choose up to 5 ingredients:'
@@ -30,11 +30,7 @@ pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 # st.stop()
 
-#ingredients_list = st.multiselect(
- # 'Choose up to 5 ingredients:'
-#  , my_dataframe
-#  , max_selections=5
-#)
+
 
 if ingredients_list:
   ingredients_string = ''
