dde826bc8c9769a0ab6a12dfa7967dad50778360
MaiaWard1 <maiaward@kubrickgroup.com>
2026-04-15 10:20:49 +0100

    Update streamlit_app.py
    indents

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,27 +25,27 @@ st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
-        'Choose up to 5 ingredients:'
-        , my_dataframe
-        , max_selections=5
+  'Choose up to 5 ingredients:'
+  , my_dataframe
+  , max_selections=5
 )
 
 if ingredients_list:
   ingredients_string = ''
   
   for fruit_chosen in ingredients_list:
-      ingredients_string += fruit_chosen + ' '
+    ingredients_string += fruit_chosen + ' '
 
-      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
     
-      st.subheader(fruit_chosen + ' Nutrition Information')
-      smoothiefroot_response = requests.get("[https://my.fruityvice.com/api/fruit/" + fruit_chosen)  
-      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    st.subheader(fruit_chosen + ' Nutrition Information')
+    smoothiefroot_response = requests.get("[https://my.fruityvice.com/api/fruit/" + fruit_chosen)  
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
  
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                    values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+      values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
     #st.write(my_insert_stmt)
     #st.stop()    
@@ -53,9 +53,9 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+      session.sql(my_insert_stmt).collect()
     
-        st.success(f"Your Smoothie is ordered!", icon="✅")
+      st.success(f"Your Smoothie is ordered!", icon="✅")
 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
